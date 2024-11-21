5eca758872cc11a520101866ad99eef272332c01
Nathan Maria da Silva <nathanms2000@outlook.com>
2024-11-20 23:39:07 -0300

    subindo projeto

---
+++ CreateUrlLambda/src/main/java/com/nathandev/createUrlShortner/UrlData.java
@@ -0,0 +1,13 @@
+package com.nathandev.createUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
